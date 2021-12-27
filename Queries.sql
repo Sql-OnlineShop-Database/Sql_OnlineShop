8dfd6fa9af7b6554090dff040a752c2c3ce09ba7
MohammadLotfi79 <mohammad.lotfi7913@gmail.com>
2021-12-27 21:55:48 +0330

    Added new queries to the database.

--- Queries.sql
+++ Queries.sql
@@ -29,7 +29,25 @@ ON CartProducts.ProductNumber = Products.ProductNumber
 Where Username = 'Sareh12'
 
 
+-- Here are new queries
+select * from Products  
+where Availablity = 0 and  CategoryNumber = 3
 
+-- Categories name for products that ordered by 'gholizade_sb' befor  '2021-06-28'
+select DISTINCT Categories.CategoryName from Categories 
+inner JOIN Products on Products.CategoryNumber = Categories.CategoryNumber
+INNER JOIN OrderProducts on OrderProducts.ProductNumber = Products.ProductNumber
+INNER JOIN Orders on Orders.OrderNumber = OrderProducts.OrderNumber
+INNER JOIN Customers on Customers.Username = Orders.Username
+WHERE Orders.OrderDate <= '2021-06-28 18:35:40.000' and Customers.Username = 'gholizade_sb'
+ 
+-- ایممیل مشتریانی که بیشتر از 50 درصد تخیف گرفته اند و مبلغ سفارش ان ها کمتر از 60000 باشد
+select Email from Orders 
+INNER JOIN Customers on Orders.Username = Customers.Username
+where Discount >= 50 and Price < 60000
 
-
-
+-- مشتریانی که حداقل 1 سفارش بالای 10 تومان ثبت کرده اند به همراه تعداد سافرششان 
+select count(*) as OrderCount,Username from Orders 
+where Price > 10
+GROUP BY Username
+having  COUNT(*) >= 1
